5898719ffb89e376dadcca92ffe98bbb51af8f54
Lohd4 <leo.daetwyler@gmail.com>
2026-08-26 11:06:44 +0200

    DUMMY TEST AND gradle changes

---
+++ src/test/java/ch/bbw/m450/tictactoe/TicTacToeMainTest.java
@@ -0,0 +1,11 @@
+package ch.bbw.m450.tictactoe;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+
+public class TicTacToeMainTest implements WithAssertions {
+    @Test
+    void ASHDJKAHDKJAH(){
+        assertThat(true );
+    }
+}
